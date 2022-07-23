1285d52d45fdb2609c616a787891fde655d0c959
tnp2004 <iceandiceisice2004@gmail.com>
2022-07-23 16:52:48 +0700

    cabcab system
    get input words and random then show display

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const closeButton = document.querySelector('#close-button');
 const settingButton = document.querySelector('#setting-button');
+const spinButton = document.querySelector('#spin-button');
+const wordsInput = document.querySelector('#words-input');
+const wordShow = document.querySelector('#word-show');
 const menu = document.querySelector('#menu');
+
+const listToSpin = () => {
+    const list = wordsInput.value.replace(/\s+/g, '/')
+    const wordArray = list.split('/');
+    return wordArray;
+}
+
+const spin = () => {
+    wordShow.innerHTML = randomWord(listToSpin(), false)? randomWord(listToSpin(), false) : 'No word';
+}
+
+const randomWord = (wordArr, isDelete) => {
+    if (wordArr) {
+        const randomIndex = Math.floor(Math.random() * wordArr.length);
+        const randomWord = wordArr[randomIndex];
+        if (isDelete) {
+            wordArr.splice(randomIndex, 1);
+        }
+        return randomWord;
+    }
+}
+
+spinButton.addEventListener('click', () => {
+    spin()
+})
 
 closeButton.addEventListener('click', () => {
     menu.classList.toggle('d-none');
